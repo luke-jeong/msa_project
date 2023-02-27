754e39584f64104eadf7be001413f144f677ccf5
luke-jeong <jgh3482@gmail.com>
2023-02-27 16:01:25 +0900

    add user service

---
+++ user_service/src/main/java/service/UserService.java
@@ -0,0 +1,7 @@
+package service;
+
+import dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
---
+++ user_service/src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package service;
+
+import dto.UserDto;
+import jpa.UserEntity;
+import jpa.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        //생성 시 id는 랜덤하게 생성되도록 설정
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_password");
+
+        userRepository.save(userEntity);
+
+        UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
+        return returnUserDto;
+    }
+}
