66342d40570ef5be82e8fdd3831113cd1f6c1f1d
luke-jeong <jgh3482@gmail.com>
2023-02-27 12:47:07 +0900

    add Eureka server

--- user_service/src/main/java/controller/UserController.java
+++ user_service/src/main/java/controller/UserController.java
@@ -1,15 +1,32 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vo.Greeting;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+    private Environment env;
+
+    @Autowired
+    private Greeting greeting;
+    @Autowired
+    public UserController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/health_check")
     public String status(){
         return "It's working in User service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 }
