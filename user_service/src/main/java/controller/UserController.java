7b1bafc59d39263a142368b538115f35b3b87742
luke-jeong <jgh3482@gmail.com>
2023-02-27 11:10:55 +0900

    user-service

---
+++ user_service/src/main/java/controller/UserController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's working in User service";
+    }
+}
