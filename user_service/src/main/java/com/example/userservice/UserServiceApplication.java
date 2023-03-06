d54e1dbc3e732f4bb544db1d306198cebf4632c6
creatong <jgh3482@gmail.com>
2023-03-06 23:42:13 +0900

    add LoadBalanced

--- user_service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user_service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -22,7 +23,9 @@ public class UserServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+	//LoadBalanced 추가하면서 user-service.yml에서 url을 order-service로 변경
 }
